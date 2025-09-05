b509f09060299e4ccd2a7ce5959e486454105059
Angel Polanco <angelepolanco325@gmail.com>
2025-09-05 13:50:43 -0400

    added script

---
+++ index.js
@@ -0,0 +1,10 @@
+let code1;
+code1 = 7 + 3;
+let code2 = 90 / 2 - 5;
+let code3 = 79 % 40;
+let message = "The vault has been secured. The combination is: ";
+let codeA = code1 + "-" + code2 + "-" + code3;
+let codeB = `${code1}-${code2}-${code3}`;
+console.log(message);
+console.log(codeA);
+console.log(codeB);
